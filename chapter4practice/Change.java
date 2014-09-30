1c2f4ace69d64bbb5e1a2d6d0bdf5f5f1b845541
zmswartz <zm@tysscz.com>
2014-09-30 11:08:50 -0500

    Unit 2 Classes
    Change practice complete

--- chapter4practice/Change.java
+++ chapter4practice/Change.java
@@ -22,6 +22,31 @@ public class Change
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter Total Cost: ");
+        double cost = s.nextDouble();
+        System.out.print("Enter Amount Recieved: ");
+        double amount = s.nextDouble();
+        double change = 100*amount - 100*cost;
+        int twenty = (int) change/2000;
+        change = change%2000;
+        int tens = (int) change/1000;
+        change = change%1000;
+        int fives = (int) change/500;
+        change = change%500;
+        int ones = (int) change/100;
+        change = change%100;
+        int quarters = (int) change/25;
+        change = change%25;
+        int dimes = (int) change/10;
+        change = change%10;
+        int nickles = (int) change/5;
+        int pennies = (int) change%5;
+        String display = "\nReturn:";
+        display += "\nTwenties: " + twenty + "\nTens: " + tens + "\nFives: " + fives;
+        display += "\nOnes: " + ones + "\nQuarters: " + quarters + "\nDimes: " + dimes;
+        display += "\nNickles: " + nickles + "\nPennies: " + pennies;
+        System.out.println(display);
+        
     }
 
 }
